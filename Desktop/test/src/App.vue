917e4976aa905c230995d1584d58efb9537a04ce
yonava <yonavossandreae@gmail.com>
2023-02-11 21:06:33 -0500

    inital commit

---
+++ Desktop/test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+</script>
+
+<style>
+.center {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ Desktop/test/src/views/Tab2Page.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div style="height: 500px"></div>
+      <ion-input :value="'for some reason, this input is slow to respond'"></ion-input>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/vue';
+</script>
